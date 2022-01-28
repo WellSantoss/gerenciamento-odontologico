3670bb1f42723676238fe1de4a8f7378a3e5992b
Wellington Santos <wellington1998santoss@gmail.com>
2022-01-28 00:17:45 -0300

    layout header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <h1>Header</h1>
+
+    <div class="profile">
+      <img src="@/assets/profile.jpg" alt="Perfil" />
+      <p><span>Mariana Ribeiro</span><br />Administrador</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  padding: 16px 32px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+h1 {
+  display: flex;
+  align-items: center;
+
+  &::before {
+    content: "";
+    display: block;
+    width: 5px;
+    height: 33px;
+    background: red;
+    margin-right: 16px;
+  }
+}
+
+.profile {
+  display: flex;
+  align-items: center;
+
+  img {
+    border-radius: 50%;
+    margin-right: 16px;
+  }
+
+  span {
+    font-weight: 500;
+  }
+}
+</style>
