897dbe5eae2a042f3bf9e52a2b31ad05432ba205
Wellington Santos <wellington1998santoss@gmail.com>
2022-02-10 23:45:31 -0300

    adicionando layouts

---
+++ src/layouts/Default.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <Menu />
+    <div class="container">
+      <Header :titulo="'Início'" />
+    </div>
+  </main>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Menu from "@/components/Menu.vue";
+
+export default {
+  name: "Default",
+  components: {
+    Header,
+    Menu,
+  },
+  mounted() {
+    const toggleMenu = document.querySelector("button.toggle-menu");
+    const menu = document.querySelector(".menu");
+
+    toggleMenu.addEventListener("click", () => {
+      menu.classList.toggle("active");
+    });
+  },
+};
+</script>
+
+<style></style>
---
+++ src/layouts/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <div>
+      <div class="header">
+        <img src="@/assets/logo.svg" alt="Logo" />
+        <h1>Login</h1>
+      </div>
+      <form action="">
+        <label for="user">Usuário</label>
+        <input type="text" name="user" id="user" />
+        <label for="password">Senha</label>
+        <input type="password" name="password" id="password" />
+        <router-link to="/">
+          <button>Entrar</button>
+        </router-link>
+      </form>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  & > div {
+    max-width: 300px;
+    flex: 1;
+  }
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 64px;
+
+  h1 {
+    margin-left: 32px;
+  }
+}
+</style>
