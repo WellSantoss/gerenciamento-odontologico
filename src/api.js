22848c4314f2823484bdd1ac45901d09477beb80
Wellington Santos <wellington1998santoss@gmail.com>
2022-05-30 00:52:52 -0300

    mudando url da api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost/gerenciamento-odontologico-api",
+  baseURL: "http://gerenciamento-odontologico-api.000webhostapp.com",
   headers: {
     Authorization: "Bearer " + localStorage.getItem("token"),
   },
